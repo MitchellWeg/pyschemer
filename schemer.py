d703c691cc31228b0d49e2649c7813848d574c1d
MitchellWeg <mitchell.w@live.nl>
2022-01-28 15:00:45 +0100

    split up describe parsing to make it more durable

--- schemer.py
+++ schemer.py
@@ -12,33 +12,42 @@ class Database:
 
     def draw(self):
         self.get_tables()
+        self.parse_tables()
         self.describe_tables()
 
-    def get_tables(self) -> [str]:
+    def get_tables(self):
         self.cursor.execute("SHOW TABLES;")
+
+    def parse_tables(self) -> [str]:
         self.table_names = [t[0] for t in self.cursor.fetchall()]
 
-        return self.tables
+        return self.table_names
+
 
     def describe_tables(self):
         all_tables = []
 
         for table in self.table_names:
-            _columns = []
             self.cursor.execute(f"DESCRIBE {table}")
-            columns_in_table = self.cursor.fetchall()
-            for col in columns_in_table:
-                column = Column(
-                    col[0],
-                    col[1],
-                    False if col[2] == 'NO' else True,
-                    col[3],
-                    col[4],
-                    col[5]
-                )
-                _columns.append(column)
+            _columns = self.parse_describe()
 
             _table = Table(table, _columns)
             all_tables.append(_table)
 
         self.tables = all_tables
+
+    def parse_describe(self):
+        columns = []
+        columns_in_table = self.cursor.fetchall()
+        for col in columns_in_table:
+            column = Column(
+                col[0],
+                col[1],
+                False if col[2] == 'NO' else True,
+                col[3],
+                col[4],
+                col[5]
+            )
+            columns.append(col) 
+
+        return columns
